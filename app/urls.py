2b71efd5d29c0a5680f099a8347b61d57759c8ab
ANDREW Biait <andrewsilva18@hotmail.com>
2024-03-13 14:45:21 +0000

    Menu editado

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from products.views import ProductListView, NewProductCreateView, ProductDetailView, ProductUpdatView, ProductDeleteView
 from accounts.views import register_view, login_view, logout_view
-from suppliers.views import SupplierCreateView
+from suppliers.views import SupplierCreateView, SupplierListView, SupplierDetailView, SupplierUpdateView, SupplierDeleteView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +18,8 @@ urlpatterns = [
     path('product/<int:pk>/update/', ProductUpdatView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('suppliers/new/', SupplierCreateView.as_view(), name='supplier_new'),
+    path('', SupplierListView.as_view(), name='supplier_list'),
+    path('<int:pk>/', SupplierDetailView.as_view(), name='supplier_detail'),
+    path('<int:pk>/update/', SupplierUpdateView.as_view(), name='supplier_update'),
+    path('<int:pk>/delete/', SupplierDeleteView.as_view(), name='supplier_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,16 +1,10 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Supplier
 from .forms import SupplierForm
 
-
-
-
-# class SupplierListView(ListView):
-#     model = Supplier
-#     template_name = 'suppliers/supplier_list.html'
 
 @method_decorator(login_required(login_url='login'), name='dispatch')#decorator
 class SupplierCreateView(CreateView):
@@ -18,3 +12,30 @@ class SupplierCreateView(CreateView):
     form_class = SupplierForm
     template_name = 'suppliers/supplier_form.html'
     success_url = reverse_lazy('supplier_list')  # Substitua 'supplier_list' pela URL que você deseja redirecionar após o cadastro
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')#decorator
+class SupplierListView(ListView):
+    model = Supplier
+    template_name = 'suppliers/supplier_list.html'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')#decorator
+class SupplierDetailView(DetailView):
+    model = Supplier
+    template_name = 'suppliers/supplier_detail.html'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')#decorator
+class SupplierUpdateView(UpdateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'suppliers/supplier_form.html'
+    success_url = reverse_lazy('supplier_list')  # Atualize para a URL correta de listagem
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')#decorator
+class SupplierDeleteView(DeleteView):
+    model = Supplier
+    template_name = 'suppliers/supplier_confirm_delete.html'
+    success_url = reverse_lazy('supplier_list')  # Atualize para a URL correta de listagem
