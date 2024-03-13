c8894eda744b9372bf80009c0e67ed72f029a948
ANDREW Biait <andrewsilva18@hotmail.com>
2024-03-13 00:29:43 +0000

    adicção dos fornecedores 2

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from suppliers.models import Supplier
 
-# Register your models here.
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'company',)
+    search_fields = ('name','company',)
+
+
+admin.site.register(Supplier, SupplierAdmin)
+
