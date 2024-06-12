e65fa6b7e84249e7ed27410542f3fbf152273d09
ANDREW SILVA DE OLIVEIRA <CJ3014916@ifspcjoad.ifspcjo.edu.br>
2024-06-11 21:33:23 -0300

    ok

--- relatorios/urls.py
+++ relatorios/urls.py
@@ -1,11 +1,23 @@
-# relatorios/urls.py
 from django.urls import path
-from .views import ManualUsuarioView
-from . import views
+from .views import (
+    gerar_relatorios, ManualUsuarioView, ResumoCotacoesView, CotacoesDepartamentoCategoriaView,
+    ComparativoPrecosFornecedorView, DesempenhoFornecedoresView, ProdutosMaisCotadosView,
+    ProdutosMaiorVariacaoPrecoView, ProdutosSemFornecedorView, PedidosAgrupadosFornecedorView,
+    HistoricoPedidosAgrupadosView
+)
 
 app_name = 'relatorios'
 
 urlpatterns = [
-    path('gerar/', views.gerar_relatorios, name='gerar_relatorios'),
+    path('gerar/', gerar_relatorios, name='gerar_relatorios'),
     path('manual/', ManualUsuarioView.as_view(), name='manual_usuario'),
+    path('resumo-cotacoes/', ResumoCotacoesView.as_view(), name='resumo_cotacoes'),
+    path('cotacoes-departamento-categoria/', CotacoesDepartamentoCategoriaView.as_view(), name='cotacoes_departamento_categoria'),
+    path('comparativo-precos-fornecedor/', ComparativoPrecosFornecedorView.as_view(), name='comparativo_precos_fornecedor'),
+    path('desempenho-fornecedores/', DesempenhoFornecedoresView.as_view(), name='desempenho_fornecedores'),
+    path('produtos-mais-cotados/', ProdutosMaisCotadosView.as_view(), name='produtos_mais_cotados'),
+    path('produtos-maior-variacao-preco/', ProdutosMaiorVariacaoPrecoView.as_view(), name='produtos_maior_variacao_preco'),
+    path('produtos-sem-fornecedor/', ProdutosSemFornecedorView.as_view(), name='produtos_sem_fornecedor'),
+    path('pedidos-agrupados-fornecedor/', PedidosAgrupadosFornecedorView.as_view(), name='pedidos_agrupados_fornecedor'),
+    path('historico-pedidos-agrupados/', HistoricoPedidosAgrupadosView.as_view(), name='historico_pedidos_agrupados'),
 ]
--- relatorios/views.py
+++ relatorios/views.py
@@ -1,11 +1,101 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.db.models import Sum, Count
+from django.http import JsonResponse, HttpResponse
+from cotacao.models import Cotacao, ItemCotacao
+import csv
 
 def gerar_relatorios(request):
     return render(request, 'relatorios/gerar_relatorios.html')
 
-
-
-
 class ManualUsuarioView(TemplateView):
     template_name = 'relatorios/manual_usuario.html'
+
+class ResumoCotacoesView(TemplateView):
+    template_name = 'relatorios/resumo_cotacoes.html'
+
+    def get(self, request):
+        # Lógica para obter dados do relatório
+        data_inicio = request.GET.get('data_inicio')
+        data_fim = request.GET.get('data_fim')
+
+        if data_inicio and data_fim:
+            cotacoes = Cotacao.objects.filter(data_abertura__gte=data_inicio, data_fechamento__lte=data_fim)
+        else:
+            cotacoes = Cotacao.objects.all()
+
+        total_cotacoes = cotacoes.count()
+        total_itens = ItemCotacao.objects.filter(cotacao__in=cotacoes).count()
+        valor_total = ItemCotacao.objects.filter(cotacao__in=cotacoes).aggregate(Sum('produto__preco_de_custo'))['produto__preco_de_custo__sum']
+
+        context = {
+            'cotacoes': cotacoes,
+            'total_cotacoes': total_cotacoes,
+            'total_itens': total_itens,
+            'valor_total': valor_total,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        # Lógica para exportar o relatório
+        data_inicio = request.POST.get('data_inicio')
+        data_fim = request.POST.get('data_fim')
+        formato = request.POST.get('formato')
+
+        if data_inicio and data_fim:
+            cotacoes = Cotacao.objects.filter(data_abertura__gte=data_inicio, data_fechamento__lte=data_fim)
+        else:
+            cotacoes = Cotacao.objects.all()
+
+        if formato == 'csv':
+            return self.exportar_csv(cotacoes)
+        elif formato == 'xml':
+            return self.exportar_xml(cotacoes)
+        elif formato == 'pdf':
+            return self.exportar_pdf(cotacoes)
+        else:
+            return JsonResponse({'erro': 'Formato inválido'})
+
+    def exportar_csv(self, cotacoes):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="resumo_cotacoes.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Nome', 'Departamento', 'Data Abertura', 'Data Fechamento', 'Status', 'Prazo'])
+
+        for cotacao in cotacoes:
+            writer.writerow([cotacao.nome, cotacao.departamento.nome, cotacao.data_abertura, cotacao.data_fechamento, cotacao.status, cotacao.prazo])
+
+        return response
+
+    def exportar_xml(self, cotacoes):
+        # Implementar exportação para XML
+        pass
+
+    def exportar_pdf(self, cotacoes):
+        # Implementar exportação para PDF
+        pass
+
+class CotacoesDepartamentoCategoriaView(TemplateView):
+    template_name = 'relatorios/cotacoes_departamento_categoria.html'
+
+class ComparativoPrecosFornecedorView(TemplateView):
+    template_name = 'relatorios/comparativo_precos_fornecedor.html'
+
+class DesempenhoFornecedoresView(TemplateView):
+    template_name = 'relatorios/desempenho_fornecedores.html'
+
+class ProdutosMaisCotadosView(TemplateView):
+    template_name = 'relatorios/produtos_mais_cotados.html'
+
+class ProdutosMaiorVariacaoPrecoView(TemplateView):
+    template_name = 'relatorios/produtos_maior_variacao_preco.html'
+
+class ProdutosSemFornecedorView(TemplateView):
+    template_name = 'relatorios/produtos_sem_fornecedor.html'
+
+class PedidosAgrupadosFornecedorView(TemplateView):
+    template_name = 'relatorios/pedidos_agrupados_fornecedor.html'
+
+class HistoricoPedidosAgrupadosView(TemplateView):
+    template_name = 'relatorios/historico_pedidos_agrupados.html'
